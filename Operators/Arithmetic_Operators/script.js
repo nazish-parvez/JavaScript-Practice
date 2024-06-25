d82317ca57d3160370c7bf682810ab6e3a114f6a
nazish-parvez <nazishparvez13@gmail.com>
2024-06-25 16:10:55 +0530

    Operators

---
+++ Operators/Arithmetic_Operators/script.js
@@ -0,0 +1,39 @@
+// Arithmetic Operators
+
+let a = 10;
+let b = 3;
+// Hera a and b are Operand. Now a + b where '+' ==> is Operator
+
+//Addition
+const addition = a + b;
+console.log(`Addition of a = ${a} + b = ${b} ==== ${addition}`);
+
+//Subtraction
+
+const subtraction = a - b;
+console.log(`Subtraction of a = ${a} - b = ${b} ==== ${subtraction}`);
+
+// Multipliction
+
+const multipliction = a * b;
+console.log(`Multipliction of a = ${a} * b = ${b} ==== ${multipliction}`);
+
+// Exponentiation
+
+/* The exponentiation operator (**) raises the first operand to the power of the second operand. */
+
+const exponentiation = a ** b;
+console.log(`Exponentiation of a = ${a} ** b = ${b} ==== ${exponentiation}`);
+
+// Division
+
+const division = a / b;
+console.log(`Division of a = ${a} / b = ${b} ==== ${division}`);
+
+// Modulus (Remainder)
+
+const modulus = a % b;
+console.log(`Modulus of a = ${a} % b = ${b} ==== ${modulus}`);
+
+
+// Increment
