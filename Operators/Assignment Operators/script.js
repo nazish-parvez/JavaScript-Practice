a3110d25842e8bc5c9b8b435d5ad7c776657f554
nazish-parvez <nazishparvez13@gmail.com>
2024-06-26 12:25:00 +0530

    Assignment Operators Added

---
+++ Operators/Assignment Operators/script.js
@@ -0,0 +1,47 @@
+// Assignment Operators
+
+// = || += || -= || *= || /= || %= || **=
+
+// x = y;
+
+//Addition Assignment Operator
+
+let AdditionAssignmentOperator = 4;
+AdditionAssignmentOperator += 2; // AdditionAssignmentOperator = AdditionAssignmentOperator + 2;
+console.log("AdditionAssignmentOperator = ", AdditionAssignmentOperator); //Output ---- 6
+
+// Subtraction Assignment Operator
+
+let SubtractionAssignmentOperator = 6;
+SubtractionAssignmentOperator -= 2; // SubtractionAssignmentOperator = SubtractionAssignmentOperator -2;
+console.log("SubtractionAssignmentOperator = ", SubtractionAssignmentOperator); // Output ---- 4
+
+// Multiplication Assignment Operator
+
+let MultiplicationAssignmentOperator = 2;
+MultiplicationAssignmentOperator *= 3; // MultiplicationAssignmentOperator = MultiplicationAssignmentOperator * 3;
+console.log(
+  "MultiplicationAssignmentOperator =",
+  MultiplicationAssignmentOperator
+); // Output ---- 6
+
+// Division Assignment Operator
+
+let DivisionAssignmentOperator = 12;
+DivisionAssignmentOperator /= 2; // DivisionAssignmentOperator = DivisionAssignmentOperator / 2;
+console.log("DivisionAssignmentOperator = ", DivisionAssignmentOperator); // Output ---- 6
+
+// Exponentiation Assignment Operator
+
+let ExponentiationAssignmentOperator = 2;
+ExponentiationAssignmentOperator **= 2; // ExponentiationAssignmentOperator = ExponentiationAssignmentOperator ** 23;
+console.log(
+  "ExponentiationAssignmentOperator = ",
+  ExponentiationAssignmentOperator
+); // Output ---- 4
+
+// Modulus (Remainder) Assignment Operator
+
+let RemainderAssignmentOperator = 12;
+RemainderAssignmentOperator %= 6; // RemainderAssignmentOperator = RemainderAssignmentOperator % 6;
+console.log("RemainderAssignmentOperator = ", RemainderAssignmentOperator); // Output ---- 0
