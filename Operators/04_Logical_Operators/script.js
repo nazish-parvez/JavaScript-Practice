cc842f38ad57d7f54b064e805a017ae28a8f87f9
nazish-parvez <nazishparvez13@gmail.com>
2024-07-02 14:28:42 +0530

    Operators

---
+++ Operators/04_Logical_Operators/script.js
@@ -0,0 +1,77 @@
+// Logical Operators
+
+// Operator     Meaning         Example         Result
+
+// &&           Logical and     (5<2)&&(5>3)    False
+// ||           Logical or      (5<2)||(5>3)    True
+// !            Logical not     !(5<2)          True
+
+// Logical and (&&)
+
+//Truth Table
+// Operand 1    Operand 2   Result
+
+// True         True        True
+// True         False       False
+// False        True        False
+// False        False       False
+
+{
+  let a = 10 > 5; // true
+  let b = 20 > 8; // true
+  let c = a && b; // true && true
+
+  console.log(c); // True
+
+  let x = 10 < 5; // false
+  let y = 20 > 8; // true
+  let z = x && y; // false && true
+
+  console.log(z); // False
+}
+
+// Logical or (||)
+
+//Truth Table
+// Operand 1    Operand 2   Result
+
+// True         True        True
+// True         False       True
+// False        True        True
+// False        False       False
+
+{
+  let a = 10 > 5; // true
+  let b = 20 < 8; // false
+  let c = a || b; // true && false
+
+  console.log(c); // True
+
+  let x = 10 < 5; // false
+  let y = 20 < 8; // false
+  let z = x || y; // false && false
+
+  console.log(z); // False
+}
+
+// Logical not (!)
+
+//Truth Table
+// Operand      Result
+
+// False        True
+// True         False
+
+{
+  let a = 10 > 5; // true
+  let b = !(10 > 5); // false
+  console.log(a); // True
+  console.log(b); // False
+}
+
+{
+  let a = 10 < 5; // false
+  let b = !(10 < 5); // true
+  console.log(a); // False
+  console.log(b); // True
+}
