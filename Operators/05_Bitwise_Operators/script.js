fb7dd4e2da687a4ee889ceeb31be27b1170000b3
nazish-parvez <nazishparvez13@gmail.com>
2024-07-10 13:38:31 +0530

    Operators

---
+++ Operators/05_Bitwise_Operators/script.js
@@ -0,0 +1,20 @@
+// Bitwise Operator
+
+// Truth Table ---- (AND , OR , XOR)
+
+//  X   Y   X & Y   X | Y   X ^ Y 
+
+//  0   0   0       0       0
+//  0   1   0       1       1
+//  1   0   0       1       1
+//  1   1   1       1       0
+
+
+
+// Bitwise AND 
+{
+    let a = 1;
+    let b = 1;
+    let c = a ^ b;
+    console.log(c);
+}
