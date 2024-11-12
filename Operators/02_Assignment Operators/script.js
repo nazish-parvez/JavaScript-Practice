ff1671e52790b32e1bf6b3d3e856b2b08e8443b5
Nazish Parvez 👨‍💻 <166379592+nazish-parvez@users.noreply.github.com>
2024-11-12 17:55:48 +0530

    Update script.js

--- Operators/02_Assignment Operators/script.js
+++ Operators/02_Assignment Operators/script.js
@@ -53,7 +53,3 @@ let RemainderAssignmentOperator = 12;
 RemainderAssignmentOperator %= 6; // RemainderAssignmentOperator = RemainderAssignmentOperator % 6;
 console.log("RemainderAssignmentOperator = ", RemainderAssignmentOperator); // Output ---- 0
 
-// Modulus (Remainder) Assignment Operator
-
-let RemainderAssignmentOperator = 12;
-
